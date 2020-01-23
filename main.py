743a667111b9abd093de582ef86a7797b4023140
HectorJaumandreu <hjaumandreu@gmail.com>
2020-01-23 23:57:44 +0000

    Adding functions

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
 class Employee:
-    def __init__(self, name, ID_num, department, Job_title):
+  def __init__(self, name, ID_num, department, Job_title):
         self.name = name
         self.ID_num = ID_num
         self.department = department
         self.Job_title = Job_title
-  def printEmployee(self)
+  def printEmployee(self):
     print (self.name, self.ID_num, self.department, self.Job_title)
 
-    class EmployeeDictionary:
-      def __init__(self, fileName)
-        self.dic = {}
-        self.fileName = fileName
-        try: EmployeeFile = open(FileName, "r")
-        except:
-          print("no encontrado")
-          return
+class EmployeeDictionary:
+  def __init__(self, fileName):
+    self.dic = {}
+    self.fileName = fileName
+    try: EmployeeFile = open(fileName, "r")
+    except:
+        print("no encontrado")
+        return
+    print("encontrado")
+    for line in EmployeeFile:
+        EmpData = line.split(",")
+        employee = Employee(EmpData[0], EmpData[1], EmpData[2], EmpData[3])
 
-        for line in EmployeeFile
-          EmpData = line.split(",")
-          employee = Employee(EmpData[0], EmpData[1], EmpData[2], EmpData[3])
+        self.dic[employee.ID_num] = employee
+    EmployeeFile.close()
 
-          self.dic[employee.ID_num] = employee
+  def closeDictionary(self):
+    EmployeeFile = open(self.fileName, "w")
+    for ID_num in self.dic:
+      employee = self.dic[ID_num]
+      EmployeeFile.write(employee.name + "," + employee.ID_num + "," + employee.department + "," + employee.Job_title)
+    EmployeeFile.close()
 
-      def closeDictionary(self)
-        EmployeeFile = open(self.fileName, "w")
-        for ID_num in self.dic:
-          employee = self.dic[ID_num]
-          EmployeeFile.write(employee.name + "," + employee.ID_num + "," + employee.department + "," + employee.Job_title)
-          
+  def addEmployee(self, employee):
+    self.dic[employee.ID_num] = employee
+
+  def searchEmployee(self, ID_num)
+    return self.dic[ID_num]
+  
+  def editEmployee(self, ID_num, name="", department="", Job_title=""):
+    employee = self.dic[ID_num]
+    if name != "":
+      
+
+
+empleado = Employee("Hector", "1", "Architecture", "Solutions Architect")
+
+diccionario = EmployeeDictionary("Socialnauta.txt")
+diccionario.addEmployee(empleado)
+diccionario.closeDictionary()
+
+
 
 
         
